5ae10fe0402c4f1d7d7bd9b1ab929e3b83f3f046
mt322006-svg <mt322006@gmail.com>
2026-07-27 12:12:33 +0500

    refactor(l10n): export shared localization helpers

--- lib/l10n/l10n_ext.dart
+++ lib/l10n/l10n_ext.dart
@@ -2,6 +2,8 @@ import 'package:flutter/widgets.dart';
 
 import 'app_localizations.dart';
 
+export 'ryadom_l10n_helpers.dart';
+
 extension RyadomL10nContext on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this);
 }
