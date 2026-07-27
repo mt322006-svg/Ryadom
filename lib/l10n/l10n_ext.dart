9775ea18a5559769621945ae97e0525781ac965c
mt322006-svg <mt322006@gmail.com>
2026-07-27 12:18:25 +0500

    chore(l10n): keep helper imports explicit

--- lib/l10n/l10n_ext.dart
+++ lib/l10n/l10n_ext.dart
@@ -2,8 +2,6 @@ import 'package:flutter/widgets.dart';
 
 import 'app_localizations.dart';
 
-export 'ryadom_l10n_helpers.dart';
-
 extension RyadomL10nContext on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this);
 }
