909327588bce2e1516adc75c241802931dd46e27
mt322006-svg <mt322006@gmail.com>
2026-09-25 08:24:42 +0500

    fix(analyze): keep services import for part widgets

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
